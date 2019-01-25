a0a41cae780177772d6b7ed7f565c2e0b6585afa
Geddy <g3ddylee@gmail.com>
2019-01-25 00:02:14 -0500

    chatrooms integration

---
+++ src/components/Chat.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="chat" v-if="channel">
+    <iframe
+      frameborder="0"
+      scrolling="yes"
+      id="chat_embed"
+      :src="'https://www.twitch.tv/embed/' + channel.name + '/chat'">
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    channel: Object
+  }
+}
+</script>
+
+<style lang="scss">
+.chat {
+  height: 100%;
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
