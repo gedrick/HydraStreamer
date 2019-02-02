e9125431ba3b25a872d6283881f7480170648445
Geddy <g3ddylee@gmail.com>
2019-02-02 15:18:39 -0500

    creation of new component

---
+++ src/components/AddStreamOverlay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="add-stream-overlay expand-to-fit">
+    <div class="add-stream-overlay__section">
+      <h2><span class="icon fa fa-list"></span> Your Followed Streamers</h2>
+    </div>
+
+    <div class="add-stream-overlay__section">
+      <h2>Your Followed Streamers</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+
+  }
+}
+</script>
+
+
+<style lang="scss">
+.add-stream-overlay {
+  display: flex;
+}
+</style>
