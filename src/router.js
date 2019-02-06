1eca16c13021bb0653c0c3bca8dd0b60118f8876
Geddy <g3ddylee@gmail.com>
2019-02-06 00:19:28 -0500

    killing this unnecessary vie

--- src/router.js
+++ src/router.js
@@ -3,25 +3,12 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Watch from './views/Watch.vue';
-import Admin from './views/Admin.vue';
 
 const requireAuthentication = (to, from, next) => {
   store.dispatch('getMe').then(() => {
     if (store.getters.isLoggedIn) {
       next();
       return;
-    }
-    next({ path: '/' });
-  });
-}
-
-const requireAdministrator = (to, from, next) => {
-  store.dispatch('getMe').then(() =>  {
-    if (store.getters.user && store.getters.user.roles) {
-      if (store.getters.user.roles.includes('administrator')) {
-        next();
-        return;
-      }
     }
     next({ path: '/' });
   });
@@ -41,12 +28,6 @@ export default new Router({
       name: 'watch',
       component: Watch,
       beforeEnter: requireAuthentication
-    },
-    {
-      path: '/admin',
-      name: '/admin',
-      component: Admin,
-      beforeEnter: requireAdministrator
     }
   ]
 });
--- src/views/Admin.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="admin">
-    hello and welcome to the admin view. it consists solely of you.
-  </div>
-</template>
